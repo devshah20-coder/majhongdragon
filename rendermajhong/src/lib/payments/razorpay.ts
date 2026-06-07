798ce0ea6e51b156795f396bb895441297ef0a5b
devshah20-coder <dev.s.shah2013@gmail.com>
2026-06-07 17:10:33 +0530

    Delete rendermajhong directory

--- rendermajhong/src/lib/payments/razorpay.ts
+++
@@ -1,6 +0,0 @@
-export function getRazorpayConfig() {
-  const keyId = process.env.RAZORPAY_KEY_ID;
-  const keySecret = process.env.RAZORPAY_KEY_SECRET;
-  if (!keyId || !keySecret) throw new Error("Razorpay credentials are missing.");
-  return { keyId, keySecret };
-}
